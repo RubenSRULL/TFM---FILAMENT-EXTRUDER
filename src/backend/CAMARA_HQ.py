5e23a4ae4c9d65e962324d5d7c85e6e874f542fd
Rubén Sahuquillo Redondo <alu0101387451@ull.edu.es>
2026-04-09 03:25:49 +0100

    configuracion camara v2

--- src/backend/CAMARA_HQ.py
+++ src/backend/CAMARA_HQ.py
@@ -7,7 +7,7 @@
 from picamera2 import Picamera2
 import time
 import cv2
-
+import numpy as np
 
 #----------------------------------------------#
 #--- Clase CAMARA_HQ para captura de imagen ---#
@@ -46,12 +46,15 @@ class CAMARA_HQ():
         video_config = self.picam2.create_video_configuration(
             main={"size": (1280, 720), "format": "RGB888"},
             controls={
-                "AeEnable": True,
-                "AwbEnable": True,
+                "AeEnable": False,          # Control exposición
+                "ExposureTime": 10000,      # Valor bajo para que solo brille el láser
+                "AnalogueGain": 1.0,        # Evitar ruido digital
+                "AwbEnable": False,         # Balance de blancos
+                "AwbGains": (1.0, 1.0),     # Evitar que el color cambie solo
                 "Brightness": 0.0,
-                "Contrast": 1.0,
-                "Sharpness": 1.0,
-                "Saturation": 1.0
+                "Contrast": 1.2,            # Subir un poco para marcar bordes
+                "Sharpness": 2.0,           # Ayuda a definir el borde del filamento
+                "Saturation": 1.5           # Resalta el rojo del láser
             }
         )
         self.picam2.configure(video_config)
@@ -68,8 +71,36 @@ class CAMARA_HQ():
             frame
         """
         while True:
+            # Captura imagen
             frame = self.picam2.capture_array()
+            # Convertir a BGR
             frame_bgr = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+            # Convertir a HSV
+            hsv = cv2.cvtColor(frame_bgr, cv2.COLOR_BGR2HSV)
+            # Rojos
+            lower_red1 = np.array([0, 120, 70])
+            upper_red1 = np.array([10, 255, 255])
+            lower_red2 = np.array([170, 120, 70])
+            upper_red2 = np.array([180, 255, 255])
+            # Máscaras
+            mask1 = cv2.inRange(hsv, lower_red1, upper_red1)
+            mask2 = cv2.inRange(hsv, lower_red2, upper_red2)
+            mask_final = mask1 + mask2
+            # Contornos rojo
+            contornos, _ = cv2.findContours(mask_final, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            for c in contornos:
+                area = cv2.contourArea(c)
+                if area > 50:
+                    x, y, w, h = cv2.boundingRect(c)
+                    y_top = y
+                    y_bottom = y + h
+                    diametro_pixeles = y_bottom - y_top
+                    diametro_mm = diametro_pixeles * 1
+                    cv2.line(frame_bgr, (x + w//2, y_top), (x + w//2, y_bottom), (255, 0, 0), 2)
+                    cv2.putText(frame_bgr, f"{diametro_mm:.2f} mm", (x + w, y + h//2), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+
+
+            # Codificación para streaming
             _, buffer = cv2.imencode('.jpg', frame_bgr, [cv2.IMWRITE_JPEG_QUALITY, 85])
             yield (b'--frame\r\n'
                 b'Content-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n')
@@ -77,5 +108,5 @@ class CAMARA_HQ():
 
 #-----Main-----#
 if __name__ == "__main__":
-    camera = CAMERA_HQ()
+    camera = CAMARA_HQ()
     camera.generate_frames()
